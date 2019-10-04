a509fdda4a41e0c8266f5fa8477db512c165836d
Jihong Kim <jk279242naver.com>
2019-10-04 01:47:02 -0500

    added navbar items that are Home and Saved Articles, and styled.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,14 +1,30 @@
 import React from "react";
 import "./style.css";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand" href="/">
-        ManUTD Scraper
-      </a>
-    </nav>
-  );
-}
+// function Nav() {
+//   return (
+//     <nav className="navbar navbar-dark bg-dark">
+//       <a className="navbar-brand" href="/">
+//         ManUTD Scraper
+//       </a>
+//     </nav>
+//   );
+// }
 
-export default Nav;
+const Navbar = props => (
+  <header className="navbar">
+    <nav className="navbar__navigation">
+      <div></div>
+      <div className="navbar__logo"><a href="/">ManUTD Scraper</a></div>
+      <div className="spacer"></div>
+      <div className="navbar__navigation-items">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Saved Articles</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Navbar;
