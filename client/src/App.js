d684108d1bbffca4aa81e3f1fc0ecc22c8501896
Jihong Kim <jk279242naver.com>
2019-10-05 19:19:03 -0500

    Look at right underneath Export Default App in App.js. I left a commit msg that was too long to commit.10.05.2019.Saturday.3:30pm

--- client/src/App.js
+++ client/src/App.js
@@ -72,3 +72,12 @@ class App extends Component {
 }
 
 export default App;
+
+// Commit msg for 10/05/2019 at around 3:30pm
+// 1. Created models directory with Article.js, index.js, and Note.js. so I could use Mongoose. 
+// 2.Worked on my scrape route in Server.js to have those data as an anchor tag. 
+// 3. By creating Utils directory with API.js, I could export an object with a method of getArticles, 
+// 4. Used componentDidMount to show the scraped articles on the react webpage. 
+// 5. Created ArticleBody Component so the data lies on it. 
+// 6. Created ArticleCard compoment so each of title and link could be inside of each card. 
+// 7. Out of those links, there were some unclickable ones with javscript:void and ones starting with slash for my source site's local webpage. So I needed to use Filter and IF statement to only filter out for those working links.
