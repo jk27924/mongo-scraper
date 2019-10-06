4a805dd0bea7454dcdf7fc84ac7a0d1d4c10aef8
Jihong Kim <jk279242naver.com>
2019-10-05 21:32:50 -0500

    styled Jumbotron with an img.

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Jumbotron.css";
+import JumbotronImage from "../../jumbotronImage.png";
 
 function Jumbotron() {
   return (
     <div className="jumbotron text-center">
-      {/* <img source={ ("../../../assets/images/jumbotron.png") }></img> */}
       <h1>MANCHESTER UNITED FC SCRAPER</h1>
-      <a target="_blank" rel="noopener noreferrer" href="https://www.manutd.com">
-        Powered By Manchester United Official Webpage
-      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.manutd.com">Powered By Manchester United Official Webpage</a>
+      <img src={JumbotronImage} alt="ManUTD Jumbotron"/>
     </div>
   );
 }
